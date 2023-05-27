aa7b1d34eef454172c95ab0d9ba115977b0c7439
Firda Putri Azzahra <101630185+FirdaAzzahra12@users.noreply.github.com>
2023-05-27 10:55:22 +0700

    menambahkan sharp
    menambahkan fungsi sharp untuk merubah ukuran gambar hero

--- sharp.js
+++ sharp.js
@@ -2,8 +2,8 @@ const sharp = require('sharp');
 const fs = require('fs');
 const path = require('path');
 
-const target = path.resolve(__dirname, 'src/public/hero');
-const destination = path.resolve(__dirname, 'src/public/hero-image');
+const target = path.resolve(__dirname, 'src/public/images/hero');
+const destination = path.resolve(__dirname, 'src/public/images/hero-image');
 
 if (!fs.existsSync(destination)) {
   fs.mkdirSync(destination);
